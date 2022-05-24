c755dec67f6122cc61108e6e7386cd32cfb51f06
Amit Kumar <72341567+amit2111github@users.noreply.github.com>
2022-05-24 22:13:50 +0530

    Update backend.js

--- src/backend.js
+++ src/backend.js
@@ -1,3 +1,3 @@
 export const API = process.env.REACT_APP_API;
 export const SOCKET_URL = process.env.REACT_APP_SOCKET_URL;
-console.log(process.env.REACT_APP_API);
+// console.log(process.env.REACT_APP_API);
